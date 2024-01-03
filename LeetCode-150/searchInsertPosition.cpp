d1cbd06ab23f8e5f5c0a00f6a03656e5766c7609
arya <aryapunni@gmail.com>
2024-01-03 11:19:26 -0500

    leet cod question

--- LeetCode-150/searchInsertPosition.cpp
+++ LeetCode-150/searchInsertPosition.cpp
@@ -1,3 +1,9 @@
+//Leet code problem 35 - Given a sorted array of distinct integers and a target value, 
+//return the index if the target is found. 
+//If not, return the index where it would be if it were inserted in order.
+//You must write an algorithm with O(log n) runtime complexity.
+
+
 #include <cctype>
 #include <cstdlib>
 #include <iostream>

808c4fb14de6604cbee7d1b1755e87f417d008ce
arya <aryapunni@gmail.com>
2024-01-09 18:08:38 -0500

    new solved problem from sliding window pattern

---
+++ patterns/slidingWindow3.cpp
@@ -0,0 +1,12 @@
+//Given an array of positive numbers and a positive number ‘k’, 
+//find the maximum sum of any contiguous subarray of size ‘k’.
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(void) {
+
+    return 0;
+}
